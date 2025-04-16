22d8dee775f5b5f30967efb52a17578db2ed4cd6
helloredreyes <hello.silentlymine@gmail.com>
2025-04-16 17:30:04 +0700

    add packageCode validation

--- routes/bookings.js
+++ routes/bookings.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { body } = require('express-validator'); 
 const router = express.Router();
 const { ensureAuthenticated } = require('../middleware/auth');
+const db = require('../db'); // Import your database connection
 
 const bookingsController = require('../controllers/bookingsController');
 
@@ -16,7 +17,18 @@ router.post('/', ensureAuthenticated, [
     body('price').isCurrency({ symbol: '$', allow_negatives: false }).withMessage('Price must be in USD'), // Updated field name
     body('discount').isFloat({ min: 0, max: 100 }).withMessage('Discount must be a percentage between 0 and 100'),
     body('paymentStatus').isIn(['paid', 'unpaid']).withMessage('Payment status must be either "paid" or "unpaid"'),
-    body('packageCode').isAlphanumeric().withMessage('Package code must be alphanumeric'),
+    body('packageCode').custom(async (value) => {
+        const packageQuery = 'SELECT 1 FROM packages WHERE packageCode = $1';
+        const promotionQuery = 'SELECT 1 FROM promotions WHERE packageCode = $1';
+
+        const packageExists = await db.query(packageQuery, [value]);
+        const promotionExists = await db.query(promotionQuery, [value]);
+
+        if (packageExists.rowCount === 0 && promotionExists.rowCount === 0) {
+            throw new Error('Package code does not exist in packages or promotions');
+        }
+        return true;
+    }),
     body('startDate').matches(/^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/).withMessage('Start date must be in mm/dd/yyyy format'),
     body('endDate').matches(/^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/).withMessage('End date must be in mm/dd/yyyy format')
 
@@ -29,7 +41,18 @@ router.put('/:id', ensureAuthenticated, [
     body('price').isCurrency({ symbol: '$', allow_negatives: false }).withMessage('Price must be in USD'), // Updated field name
     body('discount').isFloat({ min: 0, max: 100 }).withMessage('Discount must be a percentage between 0 and 100'),
     body('paymentStatus').isIn(['paid', 'unpaid']).withMessage('Payment status must be either "paid" or "unpaid"'),
-    body('packageCode').isAlphanumeric().withMessage('Package code must be alphanumeric'),
+    body('packageCode').custom(async (value) => {
+        const packageQuery = 'SELECT 1 FROM packages WHERE packageCode = $1';
+        const promotionQuery = 'SELECT 1 FROM promotions WHERE packageCode = $1';
+
+        const packageExists = await db.query(packageQuery, [value]);
+        const promotionExists = await db.query(promotionQuery, [value]);
+
+        if (packageExists.rowCount === 0 && promotionExists.rowCount === 0) {
+            throw new Error('Package code does not exist in packages or promotions');
+        }
+        return true;
+    }),
     body('startDate').matches(/^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/).withMessage('Start date must be in mm/dd/yyyy format'),
     body('endDate').matches(/^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/).withMessage('End date must be in mm/dd/yyyy format')
 
